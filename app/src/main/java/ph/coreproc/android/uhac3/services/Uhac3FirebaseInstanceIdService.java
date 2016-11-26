950cd887a00046a2798f84c2fcaf41d54082aa84
John Eris Villanueva <eris.villanueva@coreproc.ph>
2016-11-27 02:25:00 +0800

    Integrated Firebase

---
+++ app/src/main/java/ph/coreproc/android/uhac3/services/Uhac3FirebaseInstanceIdService.java
@@ -0,0 +1,28 @@
+package ph.coreproc.android.uhac3.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by johneris on 24/11/2016.
+ */
+
+public class Uhac3FirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private final String TAG = "FirebaseIdService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+
+}
---
+++ app/src/main/java/ph/coreproc/android/uhac3/services/Uhac3FirebaseMessagingService.java
@@ -0,0 +1,51 @@
+package ph.coreproc.android.uhac3.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ph.coreproc.android.uhac3.R;
+import ph.coreproc.android.uhac3.ui.main.MainActivity;
+
+/**
+ * Created by johneris on 24/11/2016.
+ */
+
+public class Uhac3FirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        sendNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("body"));
+    }
+
+    private void sendNotification(String messageTitle, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long[] pattern = {500, 500, 500, 500, 500};
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE, 1, 1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
